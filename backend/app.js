5fdc9f010ec3e72bb7d15ef31ec9822da3ddba9e
Abel <josephine.abel@outlook.fr>
2022-03-17 14:46:25 +0100

    add multer packgage

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const morgan = require('morgan');
 const sequelize = require('./config/db');
+const path = require('path');
 
 const userRoutes = require('./routes/userRoute');
 const postsRoutes = require('./routes/postsRoute');
@@ -13,6 +14,9 @@ app
     .use(express.json());
 
 sequelize.initDb();
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 // app.get('/ok', (req,res) => {
